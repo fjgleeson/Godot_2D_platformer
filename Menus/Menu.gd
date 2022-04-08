453c3d3598fa5f0f10f51b3ab1e88cae18b1c64a
Mjones99 <99313301+Mjones99@users.noreply.github.com>
2022-04-08 02:04:31 -0400

    everything must be in a folder >_>

---
+++ Menus/Menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/Start.grab_focus()
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Level/Level 0.tscn") # This will lead to beginning of game, place level here
+
+func _on_Exit_pressed():
+	get_tree().quit()
