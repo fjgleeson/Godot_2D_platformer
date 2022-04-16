18f57cd02673b2ecfe3eada6fea6e18b46600454
Mjones99 <99313301+Mjones99@users.noreply.github.com>
2022-04-16 12:38:31 -0400

    redid the hit box on the samurai

--- Player_Character/Samurai_player/Samurai_player.gd
+++ Player_Character/Samurai_player/Samurai_player.gd
@@ -26,6 +26,13 @@ var jumps = 0
 
 var is_jumping = false
 
+
+var elapsed_seconds = 0
+
+var reset = 3
+
+
+
 func damage(amount):
 	_set_health(health - amount)
 
@@ -66,22 +73,28 @@ func _physics_process(delta):
 			motion.x += ACCEL
 			facing_right = true
 			$AnimatedSprite.play("run")
-			$Slash/right.disabled = true
-			$Slash/left.disabled = true
+			$SlashR/right.disabled = true
+			$SlashR/lower.disabled = true
+			$SlashL/left.disabled = true
+			$SlashL/lower.disabled = true
 			
 		elif Input.is_action_pressed("left") && isAttacking == false:
 			motion.x -= ACCEL
 			facing_right = false
 			$AnimatedSprite.play("run")
-			$Slash/right.disabled = true
-			$Slash/left.disabled = true
+			$SlashR/right.disabled = true
+			$SlashR/lower.disabled = true
+			$SlashL/left.disabled = true
+			$SlashL/lower.disabled = true
 		else:
 			motion.x = lerp(motion.x,0,0.6)
 			
 			if (isAttacking == false):
 				$AnimatedSprite.play("idle")
-				$Slash/right.disabled = true
-				$Slash/left.disabled = true
+				$SlashR/right.disabled = true
+				$SlashR/lower.disabled = true
+				$SlashL/left.disabled = true
+				$SlashL/lower.disabled = true
 		if Input.is_action_just_pressed("mouse_left_click"):
 			if is_jumping == false:
 				isAttacking = true
@@ -89,12 +102,13 @@ func _physics_process(delta):
 					
 					#self.position.x -= 20
 					$AnimatedSprite.play("attack1")
-				
-					$Slash/left.disabled = false
+					$SlashL/left.disabled = false
+					$SlashL/lower.disabled = false
 				else:
 					$AnimatedSprite.play("attack1")
+					$SlashR/right.disabled = false
+					$SlashR/lower.disabled = false
 				
-					$Slash/right.disabled = false
 				
 				
 		if Input.is_action_just_pressed("mouse_right_click"):
@@ -104,13 +118,13 @@ func _physics_process(delta):
 					
 					#$Slash/CollisionShape2D.position.x = $Slash/CollisionShape2D.position.x * -1
 					$AnimatedSprite.play("attack2")
-				
-					$Slash/left.disabled = false
+					$SlashL/left.disabled = false
+					$SlashL/lower.disabled = false
 				else:
 					
 					$AnimatedSprite.play("attack2")
-				
-					$Slash/right.disabled = false
+					$SlashR/right.disabled = false
+					$SlashR/lower.disabled = false
 		if is_on_floor():
 			
 			is_jumping = false
@@ -141,8 +155,18 @@ func _physics_process(delta):
 		$AnimatedSprite.play("killed")
 		$hitbox.disabled = true
 		
+		
+		elapsed_seconds += delta
+		if elapsed_seconds > reset:
+			elapsed_seconds = 0
+			get_tree().change_scene("res://Menus/Menu.tscn")
+		
 
-
+func getHealth():
+	if self.health == 1:
+		return 1
+	else:
+		return 0
 
 func _on_Area2D_body_entered(body):
 	if "Enemy" in body.name:
@@ -152,6 +176,13 @@ func _on_Area2D_body_entered(body):
 
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation == "attack1" || $AnimatedSprite.animation == "attack2":
-		isAttacking = false; # Replace with function body.
-		$Slash/right.disabled = true
-		$Slash/left.disabled = true
+		isAttacking = false
+		$SlashR/right.disabled = true
+		$SlashR/lower.disabled = true
+		$SlashL/left.disabled = true
+		$SlashL/lower.disabled = true
+
+
+func _on_SlashR_body_entered(body):
+	if "Enemy" in body.name:
+		body.damage(1)
