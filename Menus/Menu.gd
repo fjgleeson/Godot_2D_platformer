f9b431b9982f04d5ebdf9196ce2c2ebf50b72d02
Mjones99 <99313301+Mjones99@users.noreply.github.com>
2022-04-16 12:38:55 -0400

    tweak the menu to be centered

--- Menus/Menu.gd
+++ Menus/Menu.gd
@@ -5,7 +5,7 @@ func _ready():
 
 
 func _on_Start_pressed():
-	get_tree().change_scene("res://Level/Level 0.tscn") # This will lead to beginning of game, place level here
+	get_tree().change_scene("res://Level/Level 0/Level 0.tscn") # This will lead to beginning of game, place level here
 
 func _on_Exit_pressed():
 	get_tree().quit()
